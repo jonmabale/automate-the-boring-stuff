376c568b15c1cd1d16150906c547f2449ca84dfe
Jon Mabale <jon.mabale@me.com>
2019-06-28 23:11:26 -0700

    completed chap 06

--- projects/table-printer.py
+++ projects/table-printer.py
@@ -2,18 +2,36 @@
 
 # table-printer.py
 
-tableData = [
+
+def print_table(table):
+    # Create a list containing the same number of 0 values as
+    # the number of inner lists in tableData
+    col_width = [0] * len(table)
+    inner_list = 0     # inner list count
+
+    # Find the longest word in each of the inner_list so that
+    # the col_width is wide enough to fit all the words
+    while inner_list < len(table):
+        for word in table_data[inner_list]:
+            if len(word) > col_width[inner_list]:
+                col_width[inner_list] = len(word)
+        inner_list += 1
+
+    # Iterate over inner_lists and print each corresponding word from
+    # each inner_lists and right aligned with the col_width value
+
+    for each_word in range(len(table[0])):
+        new_row = ''
+        for inner_list in range(len(table)):
+            new_row += ' ' + table[inner_list][each_word].rjust(
+                col_width[inner_list]) + ' '
+        print(new_row)
+
+
+table_data = [
     ['apples', 'oranges', 'cherries', 'banana'],
     ['Alice', 'Bob', 'Carol', 'David'],
-    ['dogs', 'cats', 'moose', 'goose']
+    ['dogs', 'cats', 'moose', 'goose'],
 ]
 
-
-def printTable():
-    colWidths = [0] * len(tableData)
-    for each in tableData:
-        for item in each:
-            print(item)
-
-
-printTable(tableData)
+print_table(table_data)
